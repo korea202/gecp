1fa2558ab7318b64f7667240d6c547f9ba154f46
korea202 <korea202@knou.ac.kr>
2025-10-24 18:28:09 +0900

    feat: added sample codes.

--- promptTemplate.py
+++ promptTemplate.py
@@ -4,26 +4,31 @@ from langchain_upstage import ChatUpstage
 from langchain_ollama import ChatOllama
 
 
-def process(text):
+def process(text, examples):
 
-    llm = ChatUpstage(model="solar-pro2", temperature=0.0)
+    llm = ChatUpstage(model="solar-pro2", temperature=0.1)
     #llm = ChatOllama(model="alibayram/Qwen3-30B-A3B-Instruct-2507")
     #llm = ChatOllama(model="gpt-oss:120b-cloud", temperature=0.1)
     
+    sample_section = "\n".join(
+        f"<원문>\n{src}\n<교정>\n[Answer]:{tgt}\n" for src, tgt in examples
+    )
+
+
+    print(sample_section)
+
     # 프롬프트 
     selectCategory = """
     # 지시
-    - 당신은 한국어 문장 교정 전문가입니다. 
+    - 당신은 한국어 문장 검토및 교정 전문가입니다.
     - 다음 규칙에 따라 원문을 교정하세요.
-    - 맞춤법, 띄어쓰기, 문장 부호, 문법을 자연스럽게 교정합니다.
+    - 예시를 참고하여 맞춤법, 띄어쓰기, 문장 부호, 문법을 자연스럽게 교정합니다.
     - 어떤 경우에도 설명이나 부가적인 내용은 포함하지 않습니다.
     - 오직 교정된 문장만 출력합니다.
 
+
     # 예시
-    <원문>
-    오늘 날씨가 않좋은데, 김치찌게 먹으러 갈려고.
-    <교정>
-    [Answer]:오늘 날씨가 안 좋은데, 김치찌개 먹으러 가려고.
+    {sample_section}
 
     # 교정할 문장
     <원문>
@@ -48,6 +53,7 @@ def process(text):
         | output_parser
     )
 
-    result = chain.invoke({"message": text})
+    
+    result = chain.invoke({"message": text, "sample_section": sample_section})
 
     return result
